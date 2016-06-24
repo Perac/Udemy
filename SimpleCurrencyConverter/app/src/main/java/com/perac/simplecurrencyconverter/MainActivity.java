7a7ae9310937a3383b40fd8d4f572391ec5475c8
Marko Perković <marko.perkovic3@gmail.com>
2016-06-24 16:12:36 +0200

    Finished Simple Currency Converter App

---
+++ SimpleCurrencyConverter/app/src/main/java/com/perac/simplecurrencyconverter/MainActivity.java
@@ -0,0 +1,52 @@
+package com.perac.simplecurrencyconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view) {
+        EditText et = (EditText) findViewById(R.id.valueToConvert);
+
+        double value = Double.parseDouble(et.getText().toString()) * 1.11;
+
+        Log.i("Value", et.getText().toString());
+
+        Toast.makeText(getApplicationContext(), et.getText().toString() + " Euros is " + String.valueOf(value) + " USD", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
